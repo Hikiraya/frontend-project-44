3960b485c7e66127e7d982984a32d18fc8295922
Hikiraya <9215806922tispb@gmail.com>
2023-05-11 12:58:52 +0300

    step 3

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,4 +1,6 @@
 #!/usr/bin/env node
-import UserName from '../src/cli.js';
-console.log('Welcome to the Brain Games!')
-console.log(UserName)
+import greetings from '../src/cli.js';
+
+console.log('Welcome to the Brain Games!');
+
+greetings();
--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,8 @@
 import readlineSync from 'readline-sync';
 
-export default () => {
-const name = readlineSync.question('May I have your name? ');
-console.log(`Hello, ${name}!`)
-}
+const greetings = () => {
+    const userName = readlineSync.question('May I have your name? ');
+    console.log(`${'Hello,'} ${userName}${'!'}`);
+  };
+  
+  export default greetings;
